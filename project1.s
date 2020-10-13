833ec075746e84ea1199f2da9ec82e0ca61ea108
02874712 <70228981+02874712@users.noreply.github.com>
2020-10-13 16:35:37 -0400

    Commit #2- User Input Var

--- project1.s
+++ project1.s
@@ -1,13 +1,28 @@
 .data
+input:  .asciiz "\nInput: " 	#input and output variables
 output: .asciiz "\nOutput: "
+
+userInput:	 .space 11
 
 .text
 main:
-addi $t0, $t0, 5
+
 li $v0, 4
-la $a0, output
+la $a0, input		   #ask for user input
 syscall
 
-li $v0,1
-move $a0,$t0
-syscall  
+li $v0, 8
+la $a0, userInput	   #get user input
+li $a1, 11
+syscall
+
+li $v0, 4
+la $a0, output 		   #print output
+syscall
+li $v0, 4
+la $a0, userInput
+syscall
+
+end:
+li $v0, 10				#end of program
+syscall
