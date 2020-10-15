d74992c760f2dbc79f99b428edaf1c8b70335822
02874712 <70228981+02874712@users.noreply.github.com>
2020-10-14 23:17:19 -0400

    Byte and Loop
    Begins to read in by byte and starts loop (loop - not working)

--- project1.s
+++ project1.s
@@ -1,8 +1,8 @@
 .data
-input:  .asciiz "\nInput: " 	#input and output variables
-output: .asciiz "\nOutput: "
+output: .asciiz "\nOutput: "	#output prompt
 
-userInput:	 .space 11
+userInput:	 .space 11			#input var
+userOutput: .space 4			#output var
 
 .text
 main:
@@ -10,6 +10,14 @@ main:
 li $v0, 8
 la $a0, userInput	   #get user input
 li $a1, 11
+syscall
+
+add $t5, $zero, $zero   #initializing counter for looping through string
+
+loop:
+la $t1, userInput
+lb $v0, $t5($t1) 	   #loads in first byte of string
+addi $t5, $t5, 1	   #increments by one
 syscall
 
 li $v0, 4
