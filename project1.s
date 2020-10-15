12fcdf978338a72e8cfb69a6e5a0403c98273710
02874712 <70228981+02874712@users.noreply.github.com>
2020-10-15 01:58:03 -0400

    Debug- Begin Loop
    Tries to load and output first byte of string in loop

--- project1.s
+++ project1.s
@@ -1,5 +1,3 @@
-output: .asciiz "\nOutput: "	#output prompt
-
 userInput:	 .space 11			#input var
 userOutput: .space 4			#output var
 
@@ -11,15 +9,23 @@ li $t1, 0  #set var=0 to begin loading byte
 
 li $v0, 8
 la $a0, userInput	    #get user input
-li $a1, 10
+li $a1, 11
 syscall
 
 loop:
-#li $t1, userInput
+la $t1, userInput
 add $t0, $t1, $a0 	    #adds $t1(index) to $a0(base address)
-lb $a0, 0($t1) 	   	    #loads in first byte of string
+lb $t0,($t0) 	   	    #loads in first byte of string
 addi $t5, $t5, 1	    #increments by one
 syscall
+
+li $v0, 4
+la $a0, output 			# tries to load byte and print -debug
+syscall
+li $v0, 4
+la $a0, userOutput
+syscall
+j end
 
 print_output:
 li $v0, 4
